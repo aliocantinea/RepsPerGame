0d5a75af5d15ce732e888ddd210b9bad94e19eda
Aliocantinea <Aliocantinea@gmail.com>
2023-07-06 21:04:48 -0700

    Set up room databases -Set for grouping of reps, with date and time -Exercise for grouping of exercise names -Games for grouping of games, with names and calculation value names Added Dao for -Adding or Deleting set -Upserting or Deleting exercise or game -Queries with Flow lists for All sets -Relation Queries for --Sets with exercises or games --Exercises with sets --Games with sets

--- app/src/main/java/ca/bradleydashjames/repspergame/MainActivity.kt
+++ app/src/main/java/ca/bradleydashjames/repspergame/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ca.bradleydashjames.repspergame.ui.theme.RepsPerGameTheme
 
+// following PLCodings guide to room and adapting it along the way https://www.youtube.com/watch?v=bOd3wO0uFr8
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
